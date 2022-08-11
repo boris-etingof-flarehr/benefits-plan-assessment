838578bb8de46b490d11f70b11eb72338e94dff1
Alan Lau <Alan.Lau@flarehr.com>
2022-08-11 00:55:23 +0000

    Merged PR 10692: Add compliance footer to the Financial Benefits step
    Related work items: #36073

--- src/components/steps/boosts.tsx
+++ src/components/steps/boosts.tsx
@@ -1,4 +1,4 @@
-import { FunctionalComponent } from 'preact';
+import { Fragment, FunctionalComponent } from 'preact';
 import { BackendApi } from '../../services/backend-api';
 import desktopImg from '../../assets/desktop/boosts.jpg';
 import mobileImg from '../../assets/mobile/boosts-mobile.jpg';
@@ -11,24 +11,36 @@ interface Props {
 
 const Boosts: FunctionalComponent<Props> = (props) => {
   return (
-    <StepTemplate
-      header={{ title: 'FINANCIAL BENEFITS', step: props.step }}
-      image={{ mobileSrc: mobileImg, desktopSrc: desktopImg }}
-      title="Give your pay a boost"
-      info="Make your pay work even harder for you, save on major life expenses and get rewarded whenever you spend with your Flare Card."
-      learnMoreText={[
-        'Flare Rewards spending cashbacks',
-        'Your own contactless Mastercard',
-        'Post-tax health insurance and income protection',
-        'Pre-tax deductions including child-care and super'
-      ]}
-      button={{
-        onClick: async (): Promise<void> => {
-          await BackendApi.command({ name: 'ProgressBoosts' });
-          props.onStepComplete();
-        }
-      }}
-    />
+    <Fragment>
+      <StepTemplate
+        header={{ title: 'FINANCIAL BENEFITS', step: props.step }}
+        image={{ mobileSrc: mobileImg, desktopSrc: desktopImg }}
+        title="Give your pay a boost"
+        info="Make your pay work even harder for you, save on major life expenses and get rewarded whenever you spend with your Flare Card."
+        learnMoreText={[
+          'Flare Rewards spending cashbacks',
+          'Your own contactless Mastercard',
+          'Post-tax health insurance and income protection',
+          'Pre-tax deductions including child-care and super'
+        ]}
+        button={{
+          onClick: async (): Promise<void> => {
+            await BackendApi.command({ name: 'ProgressBoosts' });
+            props.onStepComplete();
+          }
+        }}
+      />
+      <div class="mt-6 md:mt-16 max-w-[59.5rem] mx-auto text-xs leading-4 text-gray-500">
+        This material is general information only and does not consider your objectives, financial
+        situation or needs. You should consider the Product Disclosure Statement (PDS) and the
+        Target Market Determination relating to this product before making any decision. The Flare
+        Mastercard is a prepaid, reloadable Mastercard issued by EML Payment Solutions Limited ABN
+        30 131 436 532 AFSL 404131 (EML). Flare Financial Services Pty Ltd ABN 16 612 284 081 is
+        involved in the promotion and distribution of the Card and is a Corporate Authorised
+        Representative of EML. Mastercard and the circles design are registered trademarks of
+        Mastercard International Incorporated.
+      </div>
+    </Fragment>
   );
 };
 
